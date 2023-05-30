28b9092157a7104ce54f51b2290673ed5fdb6e8b
Krswhite <524933781@qq.com>
2023-05-30 14:05:03 +0800

    修正数据库，更改login函数以符合大组接口

--- app.py
+++ app.py
@@ -9,22 +9,24 @@ app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db1.sqlite3'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///user'
 
 db = SQLAlchemy(app)
 
-
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50))
-    password = db.Column(db.String(50))
+    __tablename__ = "user"
+    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    user_name = db.Column(db.String(20))
+    password = db.Column(db.String(20))
+    car_id = db.Column(db.String(20))
+    car_capacity = db.Column(db.Float)
 
-    def __init__(self, username, password):
-        self.name = username;
-        self.password = password;
+    def __init__(self, user_name, password, car_id, car_capacity):
+        self.user_name = user_name
+        self.password = password
+        self.car_id = car_id
+        self.car_capacity = car_capacity
 
-    def __repr__(self):
-        return '<User %s>' % self.username
 
 
 class UserManager:
@@ -33,17 +35,17 @@ class UserManager:
         self.tokenList = {}
 
     def login(self, name, password):
-        userCheckCount = User.query.filter_by(name=name, password=hashlib.md5(password).hexdigest()).count()
+        userCheckCount = User.query.filter_by(user_name=name, password=hashlib.md5(password).hexdigest()).count()
         # userCheckCount = db.session.query(User).get({'username': usernamePost}).count()
         token = 0
         if userCheckCount > 0:
             token = self.addToken(name)
         return [userCheckCount, token]
 
-    def register(self, name, password):
-        userCheckCount = User.query.filter_by(name=name).count()  # 在数据库内找是否已经注册
+    def register(self, name, password, car_id, cap):
+        userCheckCount = User.query.filter_by(name=name, car_id=car_id).count()  # 在数据库内找是否已经注册
         if userCheckCount <= 0:
-            db.session.add(User(name, hashlib.md5(password).hexdigest()))
+            db.session.add(User(name, hashlib.md5(password).hexdigest(), car_id, cap))
             db.commit()
         return userCheckCount
 
@@ -56,14 +58,12 @@ class UserManager:
 
 userManager = UserManager()
 
-# db.create_all()
-with app.app_context():
-    db.create_all()
-username = "test"
 
 
 @app.route('/')
 def test():
+    db.session.add(User("1", "1", "1", 1))
+    db.session.commit()
     pass
     # 此处可以展示网页
     # return render_template('index1.html')
@@ -99,27 +99,64 @@ def register():
 
 
 # 此方法处理用户登录
-@app.route('/user/log', methods=['POST'])
+@app.route('/user/login', methods=['POST'])
 def log():
-    usernamePost = request.form['username']
+    usernamePost = request.form['user_name']
     passwordPost = request.form['password']
-    userCheckCount = userManager.login(usernamePost, passwordPost)[0]
-    # userCheckCount = db.session.query(User).get({'username': usernamePost}).count()
-    if userCheckCount > 0:
+    userCheckCount = userManager.login(usernamePost, passwordPost)
+    if userCheckCount[0] > 0:
         return jsonify({
             "code": 1,
             "message": "success",
             "data": {
                 "user_id": 1,
                 "user_name": "John Doe",
-                "token": "12345"}
+                "token": userCheckCount[1],
+                "car_id": 1,
+                "car_capacity": 1,
+            }
         })
     else:
         return jsonify({
             "code": 0,
-            "message": "用户名与密码不匹配",
+            "message": "failed",
         })
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+with app.app_context():
+    db.create_all()
+
 if __name__ == '__main__':
+
+
     app.run(host='0.0.0.0', port=80)
